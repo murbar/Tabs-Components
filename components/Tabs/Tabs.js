6c5ed0c08f5807c591fccd4acfe33e33628e7b6a
Joel Bartlett <joel@murbar.com>
2019-01-31 15:51:09 -0500

    Fix functions

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,12 +5,11 @@ class TabLink {
     // Get the custom data attribute on the Link
     this.data = element.dataset.tab;
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
-    // Using the Item element, create a new instance of the TabItem class
-    this.tabItem = document.querySelector(
+    this.itemElement = document.querySelector(
       `.tabs-item[data-tab="${this.data}"]`
     );
-    console.log(this);
+    // Using the Item element, create a new instance of the TabItem class
+    this.tabItem = new TabItem(this.itemElement);
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () => this.select());
   }
