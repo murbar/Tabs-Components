0a270f784eeed0640e51c58f379e9777b38a1873
Joel Bartlett <joel@murbar.com>
2019-01-31 15:46:46 -0500

    Complete tabLink constructor

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,34 +1,28 @@
-
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
-    
+    this.element = element;
     // Get the custom data attribute on the Link
-    // this.data;
-    
+    this.data = element.dataset.tab;
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
-    
+    this.itemElement;
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
-    
+    this.tabItem = document.querySelector(
+      `.tabs-item[data-tab="${this.data}"]`
+    );
+    console.log(this);
     // Add a click event listener on this instance, calling the select method on click
-
-  };
+    this.element.addEventListener('click', () => this.select());
+  }
 
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    
     // Call the select method on the item associated with this link
-
   }
 }
 
@@ -41,9 +35,7 @@ class TabItem {
   select() {
     // Select all ".tabs-item" elements from the DOM
     // const items;
-
     // Remove the class "tabs-item-selected" from each element
-    
     // Add a class named "tabs-item-selected" to this element
     //this.element;
   }
@@ -59,4 +51,7 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll('.tabs-link');
+links.forEach(link => {
+  return new TabLink(link);
+});
